6f19c4a87ff2e50c4b83ef34057d977c425b73c0
Elliotico <48328072+Elliotico@users.noreply.github.com>
2023-04-16 21:18:03 -0300

    Adicionado input

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-   
+    //function pegarTextoHistória() {
+    //    const campoTexto = document.querySelector("#formHistoria");
+    //    console.log(campoTexto.value);
+   // }
 
-    function receberTextoHistoria() {
-        var textoHistoria = document.getElementById("form_Historia").value;
-        console.log(textoHistoria);
+
+    function executar(){
+        var texto= document.getElementById('historia').value;
+        var lista = document.getElementById('historico');
+        var adicionar = true;
+
+        var opt = document.createElement('resultado');
+
+        for(i=0; i <lista.resultado.lenght; i++) {
+           if(texto==lista.resultado[i].value){
+               adicionar=false;
+  
+           } 
+           if(adicionar==true){
+               opt.value=texto;
+               lista.appendChild(opt);        
+               
+           }
+        
+        }
 
 
-       document.getElementById("resultado").innerHTML = textoHistoria
+    //function receberTextoHistoria() {
+     //   var textoHistoria = document.getElementById("form_Historia").value;
+      //  console.log(textoHistoria);
+
+
+    //   document.getElementById("resultado").innerHTML = textoHistoria
     
 
        //const minha_propriedade = localStorage.getItem('valor_Historia');
