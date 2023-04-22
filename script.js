977bcffe63309da98de8c6c2755711bb1b742fa1
Elliotico <48328072+Elliotico@users.noreply.github.com>
2023-04-22 01:57:43 -0300

    retornando pasta

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+    document.querySelector("form").addEventListener("submit", addToList);
+        
+    
+    
     document.getElementById("historia").addEventListener("keypress", function(event) {
         if (event.keyCode === 13) {
         event.preventDefault(); 
@@ -8,7 +12,6 @@
     function addToList() {
         event.preventDefault();
         var input = document.getElementById("historia").value;
-        
                    
         if (input === "") {
             alert("Digite algo!");
@@ -16,7 +19,7 @@
         }
            
         var listItem = document.createElement("li");
-            
+                   
         listItem.innerHTML = input;
             
         document.getElementById("myList").appendChild(listItem);
